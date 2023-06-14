edd6ce63a19b1bb2a48e1b1441d240e77804837f
Oleksandr Veleshchuk <velsashok@gmail.com>
2023-06-14 13:46:44 +0300

    Dockerize app

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,13 @@ server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
 
-server.listen({ port: 8080 }, (err, address) => {
+const { ADDRESS = "localhost", PORT = "3000" } = process.env;
+
+server.listen({ host: ADDRESS, port: parseInt(PORT, 10) }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
 
-  console.log(`Server listening at ${address}`);
+  console.log(`server listening at ${address}`);
 });
